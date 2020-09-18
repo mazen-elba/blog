16025f618897a7a692cfbb8e7f5b48e3a8d61703
Mazen Abdelbasir <mazen.elba@gmail.com>
2020-09-18 08:38:10 -0400

    re-setting backend

--- graphblog/blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from blog.models import BlogPost
-
-
-@admin.register(BlogPost)
-class BlogPostAdmin(admin.ModelAdmin):
-    """Admin for the blog post."""
-    pass
